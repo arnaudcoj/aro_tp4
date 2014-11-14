337a3db9be6ac3f32069972cfdbd93e382b515fa
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-14 11:46:17 +0100

    ajout de couleur

--- parcours.c
+++ parcours.c
@@ -5,21 +5,29 @@
 #include "sys/wait.h"
 #include <assert.h>
 
-void parcoursLargeur(tGraphe graphe);
+/* Couleurs */
+typedef enum {ROUGE=0, BLEU=1, VERT=2} tCouleur;
+typedef tCouleur *tTabCouleurs;
+
+void parcoursLargeur(tGraphe graphe, tNumeroSommet depart, int *sommetsParcourus);
 
 int main (int argc, char **argv)
 {
   tGraphe graphe;
+  tNumeroSommet depart;
+  tTabCouleurs sommetsParcourus;
+  sommetsParcourus = malloc(grapheNbSommets(graphe)*sizeof(tCouleur));
   graphe = grapheAlloue();
   assert(argc != 1);
   grapheChargeFichier(graphe, argv[1]);
-  parcoursLargeur(graphe);
+  depart = grapheChercheSommetParNom(graphe, argv[2]);
+  parcoursLargeur(graphe, depart, sommets);
   grapheLibere(graphe);
 
   return 0;
 }
 
-void parcoursLargeur(tGraphe graphe)
+void parcoursLargeur(tGraphe graphe, tNumeroSommet depart, int *sommetsParcourus)
 {
-
+  
 }
